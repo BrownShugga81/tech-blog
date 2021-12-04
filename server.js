d438020922d5e693c8fc9aa2f704449d99c91d69
Chris Zuniga <zunigachristian81@gmail.com>
2021-12-04 17:07:21 -0600

    handlebars setup

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const sequelize = require('./connection')
 
+//handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 //middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
-
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 //routing
 
 
